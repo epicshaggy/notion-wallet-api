7d91a8f7b5c15756452fe0231c9f9695cb40a28f
Jose Martinez <epicshaggy@gmail.com>
2022-08-12 11:41:58 -0400

    Removed the cors bit to see if anything changes

--- index.js
+++ index.js
@@ -5,32 +5,32 @@ const formatISO = require("date-fns/formatISO");
 
 const app = express();
 
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://gleeful-biscuit-12259f.netlify.app/"
-  );
+// app.use(function (req, res, next) {
+//   // Website you wish to allow to connect
+//   res.setHeader(
+//     "Access-Control-Allow-Origin",
+//     "https://gleeful-biscuit-12259f.netlify.app/"
+//   );
 
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
+//   // Request methods you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//   );
 
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
+//   // Request headers you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "X-Requested-With,content-type"
+//   );
 
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", false);
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader("Access-Control-Allow-Credentials", false);
 
-  // Pass to next layer of middleware
-  next();
-});
+//   // Pass to next layer of middleware
+//   next();
+// });
 
 const port = 3000;
 
