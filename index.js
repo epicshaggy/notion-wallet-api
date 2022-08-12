d62ec21dadbaa240e70320e2966c5abdfa5c66e1
Jose Martinez <epicshaggy@gmail.com>
2022-08-12 11:34:24 -0400

    add cors for client

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ const app = express();
 
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:8100");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://gleeful-biscuit-12259f.netlify.app/"
+  );
 
   // Request methods you wish to allow
   res.setHeader(
