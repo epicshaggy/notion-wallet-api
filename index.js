e0972aa7d561e0dceadad639c938971cda5770c8
Jose Martinez <epicshaggy@gmail.com>
2022-08-12 11:57:12 -0400

    possibly fixed the port issue

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
 const express = require("express");
 const { Client } = require("@notionhq/client");
 const formatISO = require("date-fns/formatISO");
-// import { Client } from "@notionhq/client";
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
 
-// app.use(function (req, res, next) {
-//   // Website you wish to allow to connect
-//   res.setHeader(
-//     "Access-Control-Allow-Origin",
-//     "https://gleeful-biscuit-12259f.netlify.app/"
-//   );
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://gleeful-biscuit-12259f.netlify.app/"
+  );
 
-//   // Request methods you wish to allow
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-//   );
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
 
-//   // Request headers you wish to allow
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "X-Requested-With,content-type"
-//   );
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
 
-//   // Set to true if you need the website to include cookies in the requests sent
-//   // to the API (e.g. in case you use sessions)
-//   res.setHeader("Access-Control-Allow-Credentials", false);
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", false);
 
-//   // Pass to next layer of middleware
-//   next();
-// });
+  // Pass to next layer of middleware
+  next();
+});
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 function getNotionClient(token) {
   const client = new Client({ auth: token });
